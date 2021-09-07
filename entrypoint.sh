6e30f4bc0a5c26da7ea6c490d796061d4961edda
appotry <andycrusoe@gmail.com>
2021-09-08 01:48:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,31 @@
+  if [ "$(ls -A /www/letsencrypt)" ]; then 
+    echo "***** /www/letsencrypt directory exists and has content, continuing *****"; 
+  else 
+    echo "***** /www/letsencrypt directory is empty. letsencrypt映射到www文件夹下持久化 *****"
+    mkdir -p /www/letsencrypt 
+    ln -s /www/letsencrypt /etc/letsencrypt 
+  fi; 
+  if [ "$(ls -A /www/init.d)" ]; then 
+    echo "***** /www/init.d directory exists and has content, continuing *****"; 
+  else 
+    echo "***** /www/init.d directory is empty *****" 
+    rm -f /etc/init.d 
+    mkdir /www/init.d 
+    ln -s /www/init.d /etc/init.d 
+    ln -s /www/letsencrypt /etc/letsencrypt 
+  fi;
+  if [ "$(ls -A /www/server/panel)" ]; then 
+    echo "***** /www/server/panel directory exists and has content, continuing *****"; 
+  else 
+    echo "***** /www/server/panel directory is empty. install panel*****" 
+    cd /tmp 
+    wget -O install_panel.sh http://download.bt.cn/install/install_panel.sh 
+    mkdir /www/init.d 
+    echo yes | bash install_panel.sh 
+    yum clean all
+  fi;
+
+
 if [ ! -f "/etc/ssh/ssh_host_rsa_key" ];then
     ssh-keygen -t rsa -N '' -q -f /etc/ssh/ssh_host_rsa_key
 fi
